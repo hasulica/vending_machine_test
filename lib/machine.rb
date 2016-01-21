2f1789e2b8c816e7da80accdab080e1c187261af
hasulica <raduasandei@gmail.com>
2016-01-21 15:42:25 +0000

    Added test for nickels, dimes, quarters and pennies

--- lib/machine.rb
+++ lib/machine.rb
@@ -1,9 +1,14 @@
 class Machine
     attr_reader :display
+    $accepted_coins = ['5', '10', '25']
     def initialize
       @display = 'INSERT COIN'
     end
     def insert coin
-      @display = '5 cents'
+      if $accepted_coins.include?(coin)
+        @display = coin + ' cents'
+      elsif coin == '1'
+        @display = 'Cannot accept pennies. Please insert coin.'
+      end
     end
 end
--- spec/machine_spec.rb
+++ spec/machine_spec.rb
@@ -8,4 +8,20 @@ describe Machine do
   it 'displays INSERT COIN when no coins inserted' do
     expect(subject.display).to eq 'INSERT COIN'
   end
+  it 'accepts nickles and displays 5 cents' do
+    subject.insert '5'
+    expect(subject.display).to eq '5 cents'
+  end
+  it 'accepts dimes and displays 10 cents' do
+    subject.insert '10'
+    expect(subject.display).to eq '10 cents'
+  end
+  it 'accepts quarters and displays 25 cents' do
+    subject.insert '25'
+    expect(subject.display).to eq '25 cents'
+  end
+  it 'does not accept pennies and displays error message' do
+    subject.insert '1'
+    expect(subject.display).to eq 'Cannot accept pennies. Please insert coin.'
+  end
 end
