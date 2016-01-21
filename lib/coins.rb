e67732604cd64a6306472ad281fc3cd7055443b8
hasulica <raduasandei@gmail.com>
2016-01-21 17:17:52 +0000

    Moved accepted coins into module. Changed coin values to integer from string.

---
+++ lib/coins.rb
@@ -0,0 +1,3 @@
+module Coins
+  ACCEPTED_COINS = [5, 10, 25]
+end
--- lib/machine.rb
+++ lib/machine.rb
@@ -1,13 +1,13 @@
+require_relative 'coins'
 class Machine
     attr_reader :display
-    $accepted_coins = ['5', '10', '25']
     def initialize
       @display = 'INSERT COIN'
     end
     def insert coin
-      if $accepted_coins.include?(coin)
-        @display = coin + ' cents'
-      elsif coin == '1'
+      if Coins::ACCEPTED_COINS.include?(coin)
+        @display = coin.to_s + ' cents'
+      elsif coin == 1
         @display = 'Cannot accept pennies. Please insert coin.'
       end
     end
